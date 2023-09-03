39c6f857e6e71f790ca8e967deb553ce1c804a62
David Ong <lipweiong@gmail.com>
2023-09-03 14:37:03 +0800

    Footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footerstyle.css"; 
+
+export const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <p>&copy; 2023 Your Website</p>
+      </div>
+    </div>
+  );
+};
